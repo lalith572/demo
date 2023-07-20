a9b3b240edcdf05dc3b1093d0b4493bd9b269176
lalith572 <a.lalithkumar582@gmail.com>
2023-07-20 18:42:51 +0530

    added function

--- add.java
+++ add.java
@@ -1,5 +1,7 @@
 package demo;
 
+import java.util.Scanner;
+
 public class add
 {
     public static void main(String agrs[])
@@ -8,8 +10,17 @@ public class add
         int b = 20;
         int c = a + b ;
         System.out.println(c);
-
         System.out.println("hellp world!!!" + "This is my first git repo");
+        mutliply(b);
+        
     }
 
+    public static double mutliply (int n){
+          
+        System.out.println("enter n value:");
+        Scanner input = new Scanner(System.in);
+        n= input.nextInt();
+        return n;
+    }
 }
+
