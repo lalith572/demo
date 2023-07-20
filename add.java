28bab070e62fa0ae7f367b32a664cdb8b6f9d1b8
Lalith Kumar A <110482152+lalith572@users.noreply.github.com>
2023-07-20 18:27:11 +0530

    Updated sout statement

--- add.java
+++ add.java
@@ -8,6 +8,8 @@ public class add
         int b = 20;
         int c = a + b ;
         System.out.println(c);
+
+        System.out.println("hellp world!!!" + "This is my first git repo");
     }
 
 }
