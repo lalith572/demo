b980d2e0df5cee34939416ff6185144417a61a8f
lalith572 <a.lalithkumar582@gmail.com>
2023-07-20 18:23:34 +0530

    first commit

---
+++ add.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class add
+{
+    public static void main(String agrs[])
+    {
+        int a = 5;
+        int b = 20;
+        int c = a + b ;
+        System.out.println(c);
+    }
+
+}
